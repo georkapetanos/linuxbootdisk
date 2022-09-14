d43409fd31cc71daf9a65c13b5ee2f2fc8d6a905
georkapetanos <hellenicglavkosmos@gmail.com>
2022-09-14 14:17:11 +0300

    updated scripts

--- librest.sh
+++ librest.sh
@@ -1,11 +1,65 @@
 #!/bin/bash
 cd source
-mkdir libudev
-cd libudev
-wget http://launchpadlibrarian.net/621833763/libudev1_249.11-0ubuntu3.5_amd64.deb
-ar vx libudev1_249.11-0ubuntu3.5_amd64.deb
+#mkdir glibc
+#cd glibc
+#wget https://launchpad.net/ubuntu/+archive/primary/+files/libc6_2.35-0ubuntu3.1_amd64.deb
+#ar vx libc6_2.35-0ubuntu3.1_amd64.deb
+#unzstd data.tar.zst
+#tar -xvf data.tar
+#cd ../
+#mkdir libudev
+#cd libudev
+#wget http://launchpadlibrarian.net/621833763/libudev1_249.11-0ubuntu3.5_amd64.deb
+#ar vx libudev1_249.11-0ubuntu3.5_amd64.deb
+#unzstd data.tar.zst
+#tar -xvf data.tar
+#cd ../
+#cd source
+#wget https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/libselinux/3.3-1build2/libselinux_3.3.orig.tar.gz
+#tar xf libselinux_3.3.orig.tar.gz
+#cd libselinux-3.3
+#make -j4
+#strip --strip-unneeded src/libselinux.so.1
+#cd ../
+#mkdir libpcre
+#cd libpcre
+#wget http://launchpadlibrarian.net/592807927/libpcre2-8-0_10.39-3build1_amd64.deb
+#ar vx libpcre2-8-0_10.39-3build1_amd64.deb
+#unzstd data.tar.zst
+#tar -xvf data.tar
+#cd ../
+#mkdir libpcre3
+#cd libpcre3
+#wget https://launchpad.net/ubuntu/+archive/primary/+files/libpcre3_8.39-13ubuntu0.22.04.1_amd64.deb
+#ar vx libpcre3_8.39-13ubuntu0.22.04.1_amd64.deb
+#unzstd data.tar.zst
+#tar -xvf data.tar
+#cd ../
+#mkdir libtinfo6
+#cd libtinfo6
+#wget http://launchpadlibrarian.net/580830585/libtinfo6_6.3-2_amd64.deb
+#ar vx libtinfo6_6.3-2_amd64.deb
+#unzstd data.tar.zst
+#tar -xvf data.tar
+#cd ../
+#mkdir libncursesw
+#cd libncursesw
+#wget http://launchpadlibrarian.net/580830583/libncursesw6_6.3-2_amd64.deb
+#ar vx libncursesw6_6.3-2_amd64.deb
+#unzstd data.tar.zst
+#tar -xvf data.tar
+#cd ../
+#mkdir ncurses-base
+#cd ncurses-base
+#wget http://launchpadlibrarian.net/580830572/ncurses-base_6.3-2_all.deb
+#ar vx ncurses-base_6.3-2_all.deb
+#unzstd data.tar.zst
+#tar -xvf data.tar
+#cd ../
+mkdir libcrypt
+cd libcrypt
+wget http://launchpadlibrarian.net/574630889/libcrypt1_4.4.27-1_amd64.deb
+ar vx libcrypt1_4.4.27-1_amd64.deb
 unzstd data.tar.zst
 tar -xvf data.tar
 cd ../
-cd source
-wget https://github.com/SELinuxProject/selinux/archive/refs/tags/3.4.tar.gz
--- mkinitramfs.sh
+++ mkinitramfs.sh
@@ -13,7 +13,8 @@ touch ./initramfs/etc/mdev.conf
 cp -a ./source/init ./initramfs/init
 chmod +x ./initramfs/init
 cp -a ./source/{group,inittab,passwd} ./initramfs/etc/
-cp -a ./source/busybox ./initramfs/bin/busybox
+#./mkbusybox.sh
+cp -a ./source/busybox-1.35.0/busybox ./initramfs/bin/busybox
 #Populate bin, sbin etc with symbolink relative links to busybox executable
 for i in $(./initramfs/bin/busybox --list-full)
 do
@@ -24,19 +25,43 @@ done
 rm ./initramfs/usr/bin/lspci
 cp ./source/pciutils-3.8.0/lspci ./initramfs/usr/bin/lspci
 cp ./source/pciutils-3.8.0/pci.ids.gz ./initramfs/usr/local/share/pci.ids.gz
+#nano
+#./mknano.sh
+cp ./source/nano-6.4/src/nano ./initramfs/usr/bin/nano
 #strace
-cp ./source/strace-5.19/src/strace ./initramfs/usr/bin/strace
+#cp ./source/strace-5.19/src/strace ./initramfs/usr/bin/strace
+#screen
+#./mkscreen.sh
+cp ./source/screen-v.4.9.0/src/screen ./initramfs/usr/bin/screen
+cp ./source/screen-v.4.9.0/src/etc/etcscreenrc ./initramfs/etc/screenrc
+#htop
+#./mkhtop.sh
+cp ./source/htop-3.2.1/htop ./initramfs/usr/bin/htop
+#net
+cp ./source/udhcpc.script ./initramfs/usr/bin/
+cp ./source/autonet.sh ./initramfs/usr/bin/
 #libs
 #./mklibc.sh
-cp ./source/glibc-2.36/glibc-build/elf/ld.so ./initramfs/usr/lib64/
-ln -s -r ./initramfs/usr/lib64/ld.so ./initramfs/usr/lib64/ld-linux-x86-64.so.2
-cp ./source/glibc-2.36/glibc-build/libc.so ./initramfs/lib/x86_64-linux-gnu/
-ln -s -r ./initramfs/lib/x86_64-linux-gnu/libc.so ./initramfs/lib/x86_64-linux-gnu/libc.so.6
+cp ./source/glibc/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 ./initramfs/usr/lib64/
+ln -s -r ./initramfs/usr/lib64/ld-linux-x86-64.so.2 ./initramfs/usr/lib64/ld.so
+cp ./source/glibc/lib/x86_64-linux-gnu/libc.so.6 ./initramfs/lib/x86_64-linux-gnu/
+ln -s -r ./initramfs/lib/x86_64-linux-gnu/libc.so.6 ./initramfs/lib/x86_64-linux-gnu/libc.so
 cp ./source/zlib-1.2.12/libz.so.1.2.12 ./initramfs/lib/x86_64-linux-gnu/
 ln -s -r ./initramfs/lib/x86_64-linux-gnu/libz.so.1.2.12 ./initramfs/lib/x86_64-linux-gnu/libz.so.1
+cp ./source/glibc/lib/x86_64-linux-gnu/libm.so.6 ./initramfs/lib/x86_64-linux-gnu/libm.so.6
 #./librest.sh
 cp ./source/libudev/usr/lib/x86_64-linux-gnu/libudev.so.1.7.2 ./initramfs/usr/lib/x86_64-linux-gnu/libudev.so.1.7.2
 ln -s -r ./initramfs/usr/lib/x86_64-linux-gnu/libudev.so.1.7.2 ./initramfs/usr/lib/x86_64-linux-gnu/libudev.so.1
+cp ./source/libselinux-3.3/src/libselinux.so.1 ./initramfs/lib/x86_64-linux-gnu/libselinux.so.1
+cp ./source/libpcre/usr/lib/x86_64-linux-gnu/libpcre2-8.so.0 ./initramfs/lib/x86_64-linux-gnu/libpcre2-8.so.0
+cp ./source/libpcre3/lib/x86_64-linux-gnu/libpcre.so.3.13.3 ./initramfs/lib/x86_64-linux-gnu/libpcre.so.3.13.3
+ln -s -r ./initramfs/lib/x86_64-linux-gnu/libpcre.so.3.13.3 ./initramfs/lib/x86_64-linux-gnu/libpcre.so.3
+cp ./source/libtinfo6/lib/x86_64-linux-gnu/libtinfo.so.6 ./initramfs/lib/x86_64-linux-gnu/libtinfo.so.6
+cp ./source/libncursesw/lib/x86_64-linux-gnu/libncursesw.so.6.3 ./initramfs/lib/x86_64-linux-gnu/libncursesw.so.6.3
+ln -s -r ./initramfs/lib/x86_64-linux-gnu/libncursesw.so.6.3 ./initramfs/lib/x86_64-linux-gnu/libncursesw.so.6
+cp -r ./source/ncurses-base/lib/terminfo ./initramfs/lib/terminfo
+cp ./source/libcrypt/lib/x86_64-linux-gnu/libcrypt.so.1.1.0 ./initramfs/lib/x86_64-linux-gnu/libcrypt.so.1.1.0
+ln -s -r ./initramfs/lib/x86_64-linux-gnu/libcrypt.so.1.1.0 ./initramfs/lib/x86_64-linux-gnu/libcrypt.so.1
 #create cpio archive
 cd initramfs
 find . | cpio -H newc -o > ../initramfs.cpio
